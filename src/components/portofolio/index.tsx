4ae8149a905c2845d8c3aa131c03a75543e635ee
darcock <ahmadhaidarelhaq@gmail.com>
2026-02-24 22:27:07 +0700

    style: redesign portofolio section

--- src/components/portofolio/index.tsx
+++ src/components/portofolio/index.tsx
@@ -1,61 +1,51 @@
 "use client"
 
-import { useState } from "react"
-
-interface PortofolioCard {
-    id: number;
-    title: string;
-    desc: string;
-}
-
 export default function PortofolioSection() {
-    const [card, setCard] = useState<number | null>(1)
-
-    const portofolio: PortofolioCard[] = [
-        {
-            id: 1,
-            title: 'Indonesia Digital Learning',
-            desc: 'Rawr'
-        },
-        {
-            id: 2,
-            title: 'Telkom DigiUp 2025',
-            desc: 'Rawr'
-        },
-        {
-            id: 3,
-            title: 'Scodule ERP Website',
-            desc: 'Warw'
-        },
-    ]
-
     return (
         <section className="container">
-            <div className="title-left">
+            <div className="title-center">
                 <h5 className="text-secondary">Portofolio</h5>
-                <h1>Project <span className="primary">I've Created</span> <br /> For My Clients</h1>
+                <h1>Project <span className="primary">I've Created</span> For My Client</h1>
             </div>
 
-            <div className="grid-container">
-                <div className="porto-left">
-                    {portofolio.map((porto) => (
-                        <div
-                            key={porto.id}
-                            className={`porto-card ${card === porto.id ? 'active' : ''}`}
-                            onClick={() => setCard(porto.id)}
-                        >
-                            <div className="porto-content">
-                                <h2 className="porto-title">{porto.title}</h2>
-                                <p className="porto-desc">{porto.desc}</p>
-                                <button className="porto-btn">
-                                    Learn More →
-                                </button>
-                            </div>
+            <div className="porto-main-container">
+                <div className="grid-container">
+                    <div className="porto-img">
+                        <img src="/dummy.svg" alt="" />
+                    </div>
+                    <div className="porto-main">
+                        <div className="porto-main-title">
+                            <h6 className="tag">WEB DESIGN</h6>
+                            <h1>Web Design for Indonesia Digital Learning.</h1>
                         </div>
-                    ))}
+                        <button className="btn-link">See Website ↗</button>
+                    </div>
                 </div>
-                <div>
 
+                <div className="grid-container">
+                    <div className="porto-main">
+                        <div className="porto-main-title">
+                            <h6 className="tag">FRONTEND</h6>
+                            <h1>Website schedule maker for Scover management.</h1>
+                        </div>
+                        <button className="btn-link">See Website ↗</button>
+                    </div>
+                    <div className="porto-img">
+                        <img src="/dummy.svg" alt="" />
+                    </div>
+                </div>
+
+                <div className="grid-container">
+                    <div className="porto-img">
+                        <img src="/dummy.svg" alt="" />
+                    </div>
+                    <div className="porto-main">
+                        <div className="porto-main-title">
+                            <h6 className="tag">WEB DESIGN</h6>
+                            <h1>Web Design for Telkom DigiUp 2025.</h1>
+                        </div>
+                        <button className="btn-link">See Website ↗</button>
+                    </div>
                 </div>
             </div>
         </section>
